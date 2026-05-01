893bac15fa9a6b3b41058ae2c6b88a3bc499740d
SakibHasanICE <hasansakib047@gmail.com>
2026-05-01 06:25:40 +0600

    create and load chroma db vector store file

--- src/vector_store.py
+++ src/vector_store.py
@@ -11,5 +11,24 @@ class VectorStoreBuilder:
     def __init__(self,csv_path:str,persist_dir:str="chroma_db"):
         self.csv_path=csv_path
         self.persist_dir=persist_dir
-        self.embedding=HuggingFaceEmbeddings(model_name="")
+        self.embedding=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    
+    def build_and_save_vectorstore(self):
+        loader=CSVLoader(
+            file_path=self.csv_path,
+            encoding="utf-8",
+            metadata_columns=[]
 
+        )
+
+        data=loader.load()
+
+        splitter= CharacterTextSplitter(chunk_size=1000,chunk_overlap=32)
+        texts= splitter.split_documents(data)
+
+        db= Chroma.from_documents(texts,self.embedding,persist_dir=self.persist_dir)
+        db.persist()
+    
+    def load_vector_store(self):
+        return Chroma(persist_directory=self.persist_dir,embedding_function=self.embedding)
+     
